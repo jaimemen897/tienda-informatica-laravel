f92d89e5798bcf9fd2cd2d16c9c64e8b3eb224da
Jorge Benavente <jorge.benavente@alumno.iesluisvives.org>
2024-02-20 20:28:02 +0100

    Return product stock on logout

--- app/Http/Controllers/Auth/EmployeeLoginController.php
+++ app/Http/Controllers/Auth/EmployeeLoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class EmployeeLoginController extends Controller
@@ -15,6 +17,27 @@ class EmployeeLoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    public function logout(Request $request)
+    {
+        $cart = session('cart', []);
+        foreach ($cart as $item) {
+            $item->product->stock += $item->quantity;
+            $item->product->save();
+        }
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect('/');
     }
 
     public function guard()
