f168bdd5192636de0238e8302ebba437cd1b7c0a
Eva <evag.251103@gmail.com>
2024-02-22 15:06:25 +0100

    estilo email

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Auth\EloquentUserProvider;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Contracts\Foundation\Application;
--- vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
+++ vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
@@ -75,13 +75,14 @@ class ResetPassword extends Notification
     protected function buildMailMessage($url)
     {
         return (new MailMessage)
-            ->subject(Lang::get('Reset Password Notification'))
-            ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::get('Reset Password'), $url)
-            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
-            ->line(Lang::get('If you did not request a password reset, no further action is required.'));
+            ->greeting(Lang::get('¡Hola!'))
+            ->subject(Lang::get('Solicitud de restablecimiento de contraseña'))
+            ->line(Lang::get('Recibes este correo electrónico porque recibimos una solicitud de restablecimiento de contraseña para tu cuenta.'))
+            ->action(Lang::get('Restablecer contraseña'), $url)
+            ->line(Lang::get('Este enlace de restablecimiento de contraseña caducará en :count minutos.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+            ->line(Lang::get('Si no solicitaste un restablecimiento de contraseña, no es necesario realizar ninguna otra acción.'))
+            ->salutation(Lang::get('¡Muchas gracias!'));
     }
-
     /**
      * Get the reset URL for the given notifiable.
      *
