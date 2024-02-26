a909a6bc14c607de6bfec3c6c005fdd12b187473
jaimemen897 <jaimemen897@gmail.com>
2024-02-26 19:57:37 +0100

    Tests client terminados

--- tests/Feature/ClientControllerTest.php
+++ tests/Feature/ClientControllerTest.php
@@ -10,39 +10,72 @@ use Illuminate\Http\UploadedFile;
 
 class ClientControllerTest extends TestCase
 {
-
     protected function setUp(): void
     {
         parent::setUp();
         $this->artisan('migrate:refresh');
         $this->artisan('db:seed');
-        $user = Employee::first();
-        $this->actingAs($user, 'employee');
     }
 
     public function testIndex()
     {
+        $user = Employee::first();
+        $this->actingAs($user, 'employee');
         $response = $this->get(route('client.index'))->assertStatus(200);
         $response->assertViewIs('clients.index');
         $response->assertViewHas('clients');
     }
 
+    public function testIndexUnauthenticated()
+    {
+        $response = $this->get(route('client.index'))->assertStatus(302);
+        $response->assertRedirect(route('login.client'));
+    }
+
     public function testShow()
     {
+        $user = Employee::first();
+        $this->actingAs($user, 'employee');
         $client = Client::first();
         $response = $this->get(route('client.show', $client->id))->assertStatus(200);
         $response->assertViewIs('clients.show');
         $response->assertViewHas('client');
     }
 
+    public function testShowUnauthenticated()
+    {
+        $client = Client::first();
+        $response = $this->get(route('client.show', $client->id))->assertStatus(302);
+        $response->assertRedirect(route('login.client'));
+    }
+
     public function testCreate()
     {
+        $user = Employee::first();
+        $this->actingAs($user, 'employee');
         $response = $this->get(route('client.create'))->assertStatus(200);
         $response->assertViewIs('clients.create');
     }
 
+    public function testCreateUnauthenticated()
+    {
+        $response = $this->get(route('client.create'))->assertStatus(302);
+        $response->assertRedirect(route('login.client'));
+    }
+
+    public function testStoreUnauthenticated()
+    {
+        $response = $this->post(route('client.store'), [
+            'name' => 'invalid',
+    ]);
+        $response->assertStatus(302);
+        $response->assertRedirect(route('login.client'));
+    }
+
     public function testStore()
     {
+        $user = Employee::first();
+        $this->actingAs($user, 'employee');
         $response = $this->post(route('client.store'), [
             'name' => 'Test Name',
             'surname' => 'Test Surname',
@@ -65,14 +98,25 @@ class ClientControllerTest extends TestCase
 
     public function testEdit()
     {
+        $user = Employee::first();
+        $this->actingAs($user, 'employee');
         $client = Client::first();
         $response = $this->get(route('client.edit', $client->id))->assertStatus(200);
         $response->assertViewIs('clients.edit');
         $response->assertViewHas('client');
     }
 
+    public function testEditUnauthenticated()
+    {
+        $client = Client::first();
+        $response = $this->get(route('client.edit', $client->id))->assertStatus(302);
+        $response->assertRedirect(route('login.client'));
+    }
+
     public function testUpdate()
     {
+        $user = Employee::first();
+        $this->actingAs($user, 'employee');
         $client = Client::first();
         $response = $this->put(route('client.update', $client->id), [
             'name' => 'Test Name',
@@ -96,22 +140,42 @@ class ClientControllerTest extends TestCase
 
     public function testProfile()
     {
+        $user = Employee::first();
+        $this->actingAs($user, 'employee');
         $client = Client::first();
         $response = $this->get(route('profile.index', $client->id))->assertStatus(200);
         $response->assertViewIs('users.profile');
         $response->assertViewHas('user');
     }
 
+    public function testProfileUnauthenticated()
+    {
+        $client = Client::first();
+        $response = $this->get(route('profile.index', $client->id))->assertStatus(302);
+        $response->assertRedirect(route('login.client'));
+    }
+
     public function testEditImage()
     {
+        $user = Employee::first();
+        $this->actingAs($user, 'employee');
         $client = Client::first();
         $response = $this->get(route('client.editImage', $client->id))->assertStatus(200);
         $response->assertViewIs('clients.image');
         $response->assertViewHas('client');
     }
 
+    public function testEditImageUnauthenticated()
+    {
+        $client = Client::first();
+        $response = $this->get(route('client.editImage', $client->id))->assertStatus(302);
+        $response->assertRedirect(route('login.client'));
+    }
+
     public function testUpdateImage()
     {
+        $user = Employee::first();
+        $this->actingAs($user, 'employee');
         $client = Client::first();
         $fakeStorage = Storage::fake('public');
         $file = UploadedFile::fake()->image('client.jpg');
@@ -132,11 +196,20 @@ class ClientControllerTest extends TestCase
 
     public function testDestroy()
     {
+        $user = Employee::first();
+        $this->actingAs($user, 'employee');
         $client = Client::first();
         $response = $this->delete(route('client.destroy', $client->id))->assertStatus(302);
         $response->assertRedirect(route('client.index'));
         $this->assertDatabaseMissing('clients', [
             'id' => $client->id,
         ]);
+    }
+
+    public function testDestroyUnauthenticated()
+    {
+        $client = Client::first();
+        $response = $this->delete(route('client.destroy', $client->id))->assertStatus(302);
+        $response->assertRedirect(route('login.client'));
     }
 }
